7ae64028a28ba9bcd215c25d221dca4aec1aa360
Sakib0017 <sakibkamal0017@gmail.com>
2024-12-04 18:08:57 +0600

    Pagination added

--- src/routers/SampleCollectionRouter.js
+++ src/routers/SampleCollectionRouter.js
@@ -6,37 +6,4 @@ sampleCollectionRouter.get("/", getSampleCollections);
 sampleCollectionRouter.post("/", createSampleCollection);
 sampleCollectionRouter.patch("/:id/status", updateSampleCollectionStatus);
 sampleCollectionRouter.get('/search', searchSample);
-sampleCollectionRouter.get('/events', async (req, res) => {
-    const { page = 1, limit = 10 } = req.query;  
-  
-    try {
-     
-      const pageNum = parseInt(page, 10);
-      const limitNum = parseInt(limit, 10);
-  
-     
-      const skip = (pageNum - 1) * limitNum;
-  
-     
-      const events = await Event.find()
-        .skip(skip)
-        .limit(limitNum);
-  
-     
-      const totalEvents = await Event.countDocuments();
-  
-     
-      res.json({
-        events,
-        pagination: {
-          totalEvents,
-          currentPage: pageNum,
-          totalPages: Math.ceil(totalEvents / limitNum),
-        },
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
 module.exports = sampleCollectionRouter;
