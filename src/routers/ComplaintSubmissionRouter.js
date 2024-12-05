fb186e8f6f21165fa99a2bb5fb0cf3152fdb13d2
Sakib0017 <sakibkamal0017@gmail.com>
2024-12-05 09:54:59 +0600

    filter deleted

--- src/routers/ComplaintSubmissionRouter.js
+++ src/routers/ComplaintSubmissionRouter.js
@@ -6,27 +6,5 @@ complaintRouter.get("/", getComplaints);
 complaintRouter.post("/", createComplaint);
 complaintRouter.patch("/:id/status", updateComplaintStatus);
 complaintRouter.get('/search', searchUsers);
-complaintRouter.get('/events', async (req, res) => {
-    const { startDate, endDate } = req.query;
-  
-    try {
-      
-      const start = new Date(startDate);
-      const end = new Date(endDate);
-  
-      
-      if (isNaN(start) || isNaN(end)) {
-        return res.status(400).json({ error: 'Invalid date format' });
-      }
-  
-      const events = await Event.find({
-        date: { $gte: start, $lte: end }
-      });
-  
-      res.json(events);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
+z
 module.exports = complaintRouter;
